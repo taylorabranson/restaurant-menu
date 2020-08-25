69a2085e504ad1a8dfbe87a83dc86b513b081dac
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-24 20:13:05 -0500

    added menuName and printMenu()

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -5,12 +5,18 @@ import java.util.Date;
 
 public class Menu {
 
+    private String menuName;
     private String lastUpdated;
-
     private ArrayList<MenuItem> menu;
 
-    private Menu(ArrayList<MenuItem> menu) {
+    public Menu(String menuName, ArrayList<MenuItem> menu) {
+        this.menuName = menuName;
         this.menu = menu;
+        menuUpdated();
+    }
+
+    public Menu(String menuName) {
+        this(menuName, new ArrayList<>());
     }
 
     private void menuUpdated() {
@@ -20,12 +26,20 @@ public class Menu {
         setLastUpdated(currentTime.toString());
     }
 
+    public String getMenuName() {
+        return this.menuName;
+    }
+
+    public void setMenuName(String aMenuName) {
+        this.menuName = aMenuName;
+    }
+
     public String getLastUpdated() {
-        return lastUpdated;
+        return this.lastUpdated;
     }
 
     private void setLastUpdated(String aLastUpdated) {
-        lastUpdated = aLastUpdated;
+        this.lastUpdated = aLastUpdated;
     }
 
     public void addMenuItem(MenuItem item) {
@@ -36,5 +50,21 @@ public class Menu {
     public void removeMenuItem(MenuItem item) {
         menu.remove(item);
         menuUpdated();
+    }
+
+    public void printMenu() {
+        System.out.println(getMenuName());
+        System.out.println("Last Updated: " + getLastUpdated());
+        for (MenuItem item : this.menu) {
+            System.out.println("\n*******\n");
+            System.out.println(item.getName());
+            if (item.isNew()) {
+                System.out.println("New Item!");
+            }
+            System.out.println("$" + item.getPrice());
+            System.out.println(item.getDescription());
+            System.out.println(item.getCategory());
+            System.out.println("Added on: " + item.getDateAddedString());
+        };
     }
 }
