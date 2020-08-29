acc6d223df2dba1355e0c310663cf63f98d92f80
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-29 18:47:54 -0500

    commit

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -2,4 +2,17 @@ package restaurant;
 
 public class Restaurant {
 
+    public static void main(String[] args) {
+
+        Menu dinnerMenu = new Menu("Dinner Menu");
+
+        dinnerMenu.addMenuItem("Pizza", 9.99, "Pepperoni and Sausage Pizza", "Entree");
+        dinnerMenu.addMenuItem("Tomato Bisque", 5.99, "Best Damn Tomato Bisque", "Soup");
+        dinnerMenu.addMenuItem("Dinner Salad", 13.99, "Spinach with Chicken", "Salad");
+
+        dinnerMenu.printMenu();
+
+
+    }
+
 }
