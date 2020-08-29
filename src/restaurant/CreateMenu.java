cca8a46590966d91201a6ff05e7ba28796db90d2
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-29 17:59:11 -0500

    commit

--- src/restaurant/CreateMenu.java
+++ src/restaurant/CreateMenu.java
@@ -4,15 +4,15 @@ public class CreateMenu {
 
     public static void main(String[] args) {
 
-        MenuItem pizza = new MenuItem("Pizza", 9.99, "Pepperoni and Sausage Pizza", "Entree");
-        MenuItem soup = new MenuItem("Tomato Bisque", 5.99, "Best Damn Tomato Bisque", "Side");
-
-
         Menu dinnerMenu = new Menu("Dinner Menu");
-        dinnerMenu.addMenuItem(pizza);
-        dinnerMenu.addMenuItem(soup);
+
+        dinnerMenu.addMenuItem("Pizza", 9.99, "Pepperoni and Sausage Pizza", "Entree");
+        dinnerMenu.addMenuItem("Tomato Bisque", 5.99, "Best Damn Tomato Bisque", "Side");
 
         dinnerMenu.printMenu();
 
+        dinnerMenu.addMenuItemFromUserInput();
+
+        dinnerMenu.printMenu();
     }
 }
