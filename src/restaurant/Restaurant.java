c5be855ae6e4638c25fc5747bc5fba38409574aa
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-29 21:28:00 -0500

    commit

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -10,9 +10,15 @@ public class Restaurant {
         dinnerMenu.addMenuItem("Tomato Bisque", 5.99, "Best Damn Tomato Bisque", "Soup");
         dinnerMenu.addMenuItem("Dinner Salad", 13.99, "Spinach with Chicken", "Salad");
 
+        System.out.println("\n PRINT MENU \n");
+
         dinnerMenu.printMenu();
 
+        System.out.println("\n PRINT SINGLE MENU ITEM \n");
 
+        dinnerMenu.printMenuItemByIndex(dinnerMenu.findMenuItemIndex("Tomato Bisque", 5.99, "Soup"));
+
+        dinnerMenu.removeMenuItemByIndex(dinnerMenu.findMenuItemIndex("Tomato Bisque", 5.99, "Soup"));
     }
 
 }
